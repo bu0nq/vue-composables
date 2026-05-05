8a30493ce36fadac0f9d5dd6ff14065c16722d0d
bu0nq <67270989+bu0nq@users.noreply.github.com>
2026-05-05 18:09:20 +0300

    Fix

--- index.ts
+++ index.ts
@@ -1,3 +1,2 @@
 export * from '#/useBemElement'
 export * from '#/useBemModifier'
-export * from '#/useStyleBackgroundImage'
--- src/useStyleBackgroundImage/index.ts
+++
@@ -1,9 +0,0 @@
-import {reactive} from 'vue'
-
-export function useStyleBackgroundImage(
-    url?: object|string
-) : {backgroundImage: string} {
-    return reactive({
-        backgroundImage: `url(${url})`
-    });
-}
